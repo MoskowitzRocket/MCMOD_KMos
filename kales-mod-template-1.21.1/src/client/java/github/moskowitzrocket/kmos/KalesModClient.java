c8d7307bc949a391ac6e486048569a87958b2e48
MoskowitzRocket <kalebug@gmail.com>
2024-08-25 18:04:58 -0700

    Added MCmod setup content

---
+++ kales-mod-template-1.21.1/src/client/java/github/moskowitzrocket/kmos/KalesModClient.java
@@ -0,0 +1,10 @@
+package github.moskowitzrocket.kmos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KalesModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ kales-mod-template-1.21.1/src/main/java/github/moskowitzrocket/kmos/KalesModDataGenerator.java
@@ -0,0 +1,11 @@
+package github.moskowitzrocket.kmos;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KalesModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
